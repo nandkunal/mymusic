68a31ce9cd0a624bb42e038ddbdeb5dd9d876773
Kunal Khaware <nandkunal@gmail.com>
2014-11-19 22:12:11 +0530

    Added Group By for pages

--- controllers/default.py
+++ controllers/default.py
@@ -23,16 +23,14 @@ def index():
 @auth.requires_login()
 def home():
     response.flash = T("Welcome to My Music!")
-    #userid=2#session['auth']['user']['id']
     fields = (db.t_music_category.cat_name, db.t_uploads.artists, db.t_uploads.upload_datetime, db.t_uploads.file_url)
-     #Define headers as tuples/dictionaries
     headers = {'t_music_category.cat_name':   'Category',
            't_uploads.artists': 'Artist',
            't_uploads.upload_datetime': 'Uploaded Date',
            't_uploads.file_url': 'File' }
 
     query=((db.t_uploads.user_id==auth.user.id) & (db.t_uploads.music_category_id==db.t_music_category.id))
-    grid = SQLFORM.grid(query,create=False, deletable=False, editable=False,headers=headers,fields=fields)
+    grid = SQLFORM.grid(query,create=False, deletable=False, editable=False,headers=headers,fields=fields,orderby=~db.t_uploads.upload_datetime)
 
     return dict(grid=grid)
     #return locals()
@@ -54,17 +52,28 @@ def upload():
   return dict(form=form)
 @auth.requires_login()
 def music():
-    query = (db.t_uploads)
-    grid = SQLFORM.grid(query,create=False, deletable=False, editable=False)
+    fields = (db.auth_user.first_name,db.t_music_category.cat_name, db.t_uploads.artists, db.t_uploads.file_url)
+    headers = {'t_music_category.cat_name':   'Category',
+           't_uploads.artists': 'Artist',
+           'auth_user.first_name': 'User',
+           't_uploads.file_url': 'File' }
+
+    query=((db.t_uploads.user_id==auth.user.id) & (db.t_uploads.music_category_id==db.t_music_category.id) & (db.auth_user.id==db.t_uploads.user_id))
+    grid = SQLFORM.grid(query,create=False, deletable=False, editable=False,headers=headers,fields=fields)
+
     return dict(grid=grid)
 
 
 
 @auth.requires_login()
 def people():
+    fields = (db.auth_user.first_name,db.auth_user.email)
+    headers = {'auth_user.first_name':   'User',
+           'auth_user.email': 'Email',
+            }
 
-    query = (db.auth_user)
-    grid = SQLFORM.grid(query,create=False, deletable=False, editable=False)
+    query=(db.auth_user.id==db.t_uploads.user_id)
+    grid = SQLFORM.grid(query,create=False, deletable=False, editable=False,headers=headers,fields=fields,groupby=db.auth_user.id)
     return dict(grid=grid)
 def user():
     """
